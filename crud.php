006ea814c98c48ddacb813420092561f88c4d58d
ZIDAN <55623766+zidan970@users.noreply.github.com>
2021-12-18 23:10:04 +0700

    Add files via upload

---
+++ crud.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Tutorial Belajar GitHub - Koding Indonesia</title>
+	</head>
+	<body>
+		<h1>Ini merupakan file html untuk latihan belajar github</h1>
+	</body>
+</html>
